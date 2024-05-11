212fb1e69a5f3fcf50da9988fbff03e2c5368243
reza <rzxsism80@gmail.com>
2024-05-11 12:37:46 +0330

    changes applied and ready to deploy

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,24 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "A Highly enthusiast Software Engineer & System programmer with deep passion for Cloud computing and distributed system enthusiast";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Software Architecture",
+  "Design Patterns",
+  "Algorithm",
+  "Data structure",
+  "Golang",
+  "C++",
+  "Python",
+  "TypeSrcipt & nest js",
+  "Postgresql, Mysql, Mongodb",
+  "Docker & Kubernetes",
+  "Linux",
 ];
 
 /**
@@ -44,8 +49,7 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote = "Most important of all above, GEEK and full time learner";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/lp.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Laptop";
 
 /**
  * Project list
@@ -31,28 +31,9 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "My CV",
+    description: "i`d be a pleasure if you check my CV to know more about me :)",
+    url: "https://drive.google.com/file/d/1Bmfk_05fPRGSPsz2gM8j8WncRwfoXj3P/view?usp=sharing",
   },
 ];
 
